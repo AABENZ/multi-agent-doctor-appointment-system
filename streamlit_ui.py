6390c52cf65a2f065f2666b1fa1a959c852198ef
AABENZ100 <ahmed.benabdallah@zinki.ai>
2026-01-08 02:33:48 +0100

    first commit

---
+++ streamlit_ui.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import requests
+
+API_URL = "http://127.0.0.1:8003/execute" 
+
+st.title("🩺 Doctor Appointment System")
+
+user_id = st.text_input("Enter your ID number:", "")
+query = st.text_area("Enter your query:", "Can you check if a dentist is available tomorrow at 10 AM?")
+
+if st.button("Submit Query"):
+    if user_id and query:
+        try:
+            response = requests.post(API_URL, json={'messages': query, 'id_number': int(user_id)}, verify=False)
+            if response.status_code == 200:
+                st.success("Response Received:")
+                response_text = response.json().get("response", "No response from server.")
+                st.text_area("System Response", value=response_text, height=150)
+            else:
+                st.error(f"Error {response.status_code}: Could not process the request.")
+        except Exception as e:
+            st.error(f"Exception occurred: {e}")
+    else:
+        st.warning("Please enter both ID and query.")
